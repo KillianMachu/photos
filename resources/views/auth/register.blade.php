761f737ff65a8d3b56d86e22f407ff92c43052c5
Elanor Filoque <elanor.filoque@gmail.com>
2023-12-04 11:18:43 +0100

    avancement du login  et register css

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,22 +2,30 @@
 
 @section('content')
 
-<form action="{{route("register")}}" method="post">
-    @csrf
-    <input type="text" name="name" required placeholder="Name" /><br />
-    <input type="email" name="email" required placeholder="Email" /><br />
-    <input type="password" name="password" required placeholder="password" /><br />
-    <input type="password" name="password_confirmation" required placeholder="password" /><br />
-    @if ($errors)
-    @error('email')
-        <p>{{$message}}</p>
-    @enderror
-    @error('password')
-        <p>{{$message}}</p>
-    @enderror
-      @endif
-    <input type="submit" /><br />
-</form>
-Déjà un compte  ? <a href="{{route("login")}}">Connectez vous</a>
-
+<div id="auth">
+    <div class="auth2">
+        <div>
+            <p>Ne t'inquiète pas, nous ne partageons pas tes données!</p>
+        </div>
+        <div>
+            <form action="{{route("register")}}" method="post">
+                @csrf
+                <input type="text" name="name" required placeholder="Name" /><br />
+                <input type="email" name="email" required placeholder="Email" /><br />
+                <input type="password" name="password" required placeholder="password" /><br />
+                <input type="password" name="password_confirmation" required placeholder="password" /><br />
+                @if ($errors)
+                @error('email')
+                    <p>{{$message}}</p>
+                @enderror
+                @error('password')
+                    <p>{{$message}}</p>
+                @enderror
+                @endif
+                <input type="submit" /><br />
+            </form>
+            Déjà un compte  ?</br> <a href="{{route("login")}}">Connectez vous !</a>
+        </div>
+    </div>
+</div>
 @endsection
