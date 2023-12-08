d126b81a9bbf79347191703eda23032869ada63c
Elanor Filoque <elanor.filoque@gmail.com>
2023-12-08 12:50:21 +0100

    css login et register fini + responsive

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,7 +13,7 @@
                 <input type="email" name="email" required placeholder="Email" /><br />
                 <input type="password" name="password" required placeholder="Mot de passe" /><br />
                 Se souvenir de moi<input type="checkbox" name="remember" id="case"/><br />
-                <input type="submit" id="bouton" placeholder="Connexion"/><br />
+                <input type="submit" id="bouton" /><br />
             </form>
             <p>Pas de compte? </br> <a href="{{route("register")}}">Inscrit toi!</a></p>
         </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,15 +5,15 @@
 <div id="auth">
     <div class="auth2">
         <div>
-            <p>Ne t'inquiète pas, nous ne partageons pas tes données!</p>
+            <h3>Ne t'inquiète pas, nous ne partageons pas tes données!</h3>
         </div>
         <div>
             <form action="{{route("register")}}" method="post">
                 @csrf
-                <input type="text" name="name" required placeholder="Name" /><br />
+                <input type="text" name="name" required placeholder="Nom   " /><br />
                 <input type="email" name="email" required placeholder="Email" /><br />
-                <input type="password" name="password" required placeholder="password" /><br />
-                <input type="password" name="password_confirmation" required placeholder="password" /><br />
+                <input type="password" name="password" required placeholder="Mot de passe" /><br />
+                <input type="password" name="password_confirmation" required placeholder="Mot de passe" /><br />
                 @if ($errors)
                 @error('email')
                     <p>{{$message}}</p>
@@ -22,9 +22,9 @@
                     <p>{{$message}}</p>
                 @enderror
                 @endif
-                <input type="submit" /><br />
+                <input type="submit" id="bouton"/><br />
             </form>
-            Déjà un compte  ?</br> <a href="{{route("login")}}">Connectez vous !</a>
+           <p>Déjà un compte  ?</br> <a href="{{route("login")}}">Connectez vous !</a></p>
         </div>
     </div>
 </div>
