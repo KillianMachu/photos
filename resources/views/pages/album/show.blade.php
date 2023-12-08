26a309c4bcc76605b4406333e10c75dd2f91fc86
duez <benoitduez1909@gmail.com>
2023-12-09 00:25:41 +0100

    ajout du css pour casiment toute les pages

--- resources/views/pages/album/show.blade.php
+++ resources/views/pages/album/show.blade.php
@@ -1,10 +1,10 @@
 @extends('app')
 
 @section('content')
-    <h1>{{$album->titre}}</h1>
-    <form action="{{route("albumFilterPhoto", "$album->id")}}" method="GET">
-        <input type="text" name="titre" id="titre">
-        <input type="text" name="tag" id="tag">
+    <h1 class="titre">{{$album->titre}}</h1>
+    <div class="album-div-flex">
+    <form action="{{route("albumFilterPhoto", "$album->id")}}" method="GET" class="form-albm">
+      
         <select name="order" id="order">
             <option value="titre">Titre</option>
             <option value="note">Note</option>
@@ -13,8 +13,11 @@
             <option value="asc">Croissant</option>
             <option value="desc">Décroissant</option>
         </select>
-        <input type="submit">
+        <input type="text" name="titre" id="titre" placeholder="titre">
+        <input type="text" name="tag" id="tag" placeholder="tag">
+        <input type="submit" class="submit">
     </form>
+</div>
     @if (isset(Auth::user()->id) && Auth::user()->id == $album->user_id)
         <form action="{{route("albumDestroy", $album->id)}}" method="post">
             @csrf
@@ -22,7 +25,7 @@
             <input type="submit" value="Supprimer l'album">
         </form>
     @endif
-    <ul>
+    <ul class="photo-album">
 
         @forelse (isset($photoFilter) && $photoFilter ? $photo=$photoFilter : $photo=$album->photos as $p)
             <li>
@@ -32,14 +35,17 @@
                         <button>Fermer</button>
                     </div>
                 </div>
-                <h2>{{$p->titre}}</h2>
+            
+                <h2 class="titre2">{{$p->titre}}</h2 >
                 <img id="photoShow" src="{{$p->url}}" alt="{{$p->titre}}">
                 <ul>
                     @foreach ($p->tags as $tag)
-                        <li><a href="{{route("tagShow", $tag->id)}}">{{$tag->nom}}</a></li>
+                        <li><a href="{{route("tagShow", $tag->id)}}" class="album-tag-show">{{$tag->nom}}</a></li>
                     @endforeach
                 </ul>
-                <p>{{$p->note}}</p>
+                <p>{{$p->note}}/5</p>
+
+
                     @if (isset(Auth::user()->id) && Auth::user()->id == $album->user_id)
                     <form action="{{route("photoDestroy", $p->id)}}" method="post">
                     @csrf
@@ -53,16 +59,17 @@
         @endforelse
     </ul>
         @if (isset(Auth::user()->id) && Auth::user()->id == $album->user_id)
-        <form action="/photo" method="POST" id="add" enctype="multipart/form-data">
+        <div class="center">
+        <form action="/photo" method="POST" id="add" enctype="multipart/form-data" class="form-create">
             @csrf
             <div class="input-fields">
-                <div>
-                    <input type="hidden" name="album_id" value="{{$album->id}}">
-                    <input type="text" name="titre-photo[]" required placeholder="Titre">
-                    <input type="file" name="image[]" required>
+                <div class="create-container">
+                    <input type="hidden" name="album_id" value="{{$album->id}}" class="menue">
+                    <input type="text" name="titre-photo[]" required placeholder="Titre"class="menue">
+                    <input type="file" name="image[]" requiredclass="menue">
                     {{-- <input type="text" name="url[]" required placeholder="Lien de l'image"> --}}
-                    <input type="number" name="note[]" min="0" max="10" required placeholder="Note">
-                    <input type="text" name="tags[]" required placeholder="Les tags">
+                    <input type="number" name="note[]" min="0" max="10" required placeholder="Note"class="menue">
+                    <input type="text" name="tags[]" required placeholder="Les tags"class="menue">
                     <button id="remove-photo">Supprimer la photo</button>
                 </div>
             </div>
@@ -70,6 +77,7 @@
             @include("partials.errors")
             <input type="submit" value="Suivant">
         </form>
+    </div>
     @endif
-    <a href="{{url()->previous()}}">Revenir en arrière</a>
+    <a href="{{url()->previous()}}" class="return">Revenir en arrière</a>
     @endsection
--- resources/views/pages/tag/index.blade.php
+++ resources/views/pages/tag/index.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
 
-<h1>Voici la liste des tags</h1>
-<ul>
+<h1  class="titre">Voici la liste des tags</h1>
+<ul class="tag-show">
     @foreach($tags as $t)
     <li><a href="{{route("tagShow",$t->id)}}">{{$t->nom}}</a></li>
     @endforeach
